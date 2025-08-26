a0e9ca57b9a3b0a5df8f181382cb4a61c3a9619d
0-Chan <kyc4421194@gmail.com>
2025-08-26 23:16:12 +0900

    feat: Car class 단위 테스트 추가

---
+++ __tests__/Car.test.js
@@ -0,0 +1,31 @@
+import Car from '../src/Car';
+
+describe('자동차 클래스', () => {
+  const car = new Car('Hyundai');
+  const expectedCarName = 'Hyundai';
+
+  it('생성 시 인수로 전달된 string과 동일한 이름을 가진다.', () => {
+    expect(car.name).toBe(expectedCarName);
+  });
+
+  it('초기 위치 값은 0이다.', () => {
+    expect(car.position).toBe(0);
+  });
+
+  it('전진할 수 있으며, 한 번에 1만큼 전진한다.', () => {
+    car.position = 0;
+
+    car.forward();
+
+    expect(car.position).toBe(1);
+  });
+
+  it('2번 전진하면 위치는 2만큼 더해진다.', () => {
+    car.position = 2;
+
+    car.forward();
+    car.forward();
+
+    expect(car.position).toBe(4);
+  });
+});
