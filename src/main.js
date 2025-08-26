9731e1f3ecdc5ac92619cf7c4888333d8ba4c6d7
0-Chan <kyc4421194@gmail.com>
2025-08-26 20:55:34 +0900

    feat: 자동차 클래스 설계

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("Car");
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  forward() {
+    this.position += 1;
+  }
+}
+
+const myCar = new Car('Hyundai');
